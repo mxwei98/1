0c1dbc09b4b8344c009375189e08e4049bfe15e8
mxwei98 <37037989+mxwei98@users.noreply.github.com>
2018-03-04 21:26:36 +0700

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
-	$('.khoi p').slideUp();
-	$('.tieude').click(function(event) {
-		$(this).next().slideToggle(400);
+	if ($('.menu').offset().top > 140)
+			$('.menu').addClass('mauhong');
+	$(window).scroll(function(event) {
+		if ($('.menu').offset().top > 140)
+			$('.menu').addClass('mauhong');
+		else $('.menu').removeClass('mauhong');
+	});
+	$('.right_menu li').on('click', function(event) {
+		var id = $(this).data("id") ;
+		$('html,body').animate({scrollTop : $(id).offset().top-80},1000,"easeInOutExpo");
 	});
 ;});
